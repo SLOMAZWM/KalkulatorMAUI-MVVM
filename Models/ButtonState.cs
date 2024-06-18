44b20719b7b6de91ee995f7d66de47c487d9bb17
Marcin Słomka <m.slomka@student.po.edu.pl>
2024-06-18 12:50:40 +0200

    Implemented a number system, and changed buttons with changed number system

---
+++ Models/ButtonState.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalkulatorMAUI_MVVM.Models
+{
+    public partial class ButtonState : ObservableObject
+    {
+        [ObservableProperty]
+        private bool _isAEnabled;
+        [ObservableProperty]
+        private bool _isBEnabled;
+        [ObservableProperty]
+        private bool _isCEnabled;
+        [ObservableProperty]
+        private bool _isDEnabled;
+        [ObservableProperty]
+        private bool _isEEnabled;
+        [ObservableProperty]
+        private bool _isFEnabled;
+        [ObservableProperty]
+        private bool _is0Enabled;
+        [ObservableProperty]
+        private bool _is1Enabled;
+        [ObservableProperty]
+        private bool _is2Enabled;
+        [ObservableProperty]
+        private bool _is3Enabled;
+        [ObservableProperty]
+        private bool _is4Enabled;
+        [ObservableProperty]
+        private bool _is5Enabled;
+        [ObservableProperty]
+        private bool _is6Enabled;
+        [ObservableProperty]
+        private bool _is7Enabled;
+        [ObservableProperty]
+        private bool _is8Enabled;
+        [ObservableProperty]
+        private bool _is9Enabled;
+    }
+
+}
--- ViewModels/ProgrammerViewModel.cs
+++ ViewModels/ProgrammerViewModel.cs
@@ -1,18 +1,17 @@
-﻿using System;
+﻿using KalkulatorMAUI_MVVM.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace KalkulatorMAUI_MVVM.ViewModels
 {
-
     public enum NumberSystem
     {
         HEX,
         DEC,
         OCT,
-        BIN
+        BIN,
     }
 
     public partial class ProgrammerViewModel : ObservableObject
@@ -25,10 +24,23 @@ namespace KalkulatorMAUI_MVVM.ViewModels
 
         private bool _firstSign = true;
 
+        [ObservableProperty]
+        private ButtonState _buttonState;
+
+        [ObservableProperty]
+        private List<NumberSystem> _numberSystems;
+
+        public ProgrammerViewModel()
+        {
+            NumberSystems = Enum.GetValues(typeof(NumberSystem)).Cast<NumberSystem>().ToList();
+            ButtonState = new ButtonState();
+            SelectedNumberSystem = NumberSystem.DEC;
+        }
+
         [RelayCommand]
         private void EnterToDisplay(string Sign)
         {
-            if(_firstSign)
+            if (_firstSign)
             {
                 Display = Sign;
                 _firstSign = false;
@@ -46,6 +58,103 @@ namespace KalkulatorMAUI_MVVM.ViewModels
             _firstSign = true;
         }
 
+        partial void OnSelectedNumberSystemChanged(NumberSystem value)
+        {
+            switch (SelectedNumberSystem)
+            {
+                case NumberSystem.HEX:
+                    EnableHexButtons();
+                    break;
+                case NumberSystem.DEC:
+                    EnableDecButtons();
+                    break;
+                case NumberSystem.OCT:
+                    EnableOctButtons();
+                    break;
+                case NumberSystem.BIN:
+                    EnableBinButtons();
+                    break;
+            }
+        }
 
+        private void EnableHexButtons()
+        {
+            ButtonState.IsAEnabled = true;
+            ButtonState.IsBEnabled = true;
+            ButtonState.IsCEnabled = true;
+            ButtonState.IsDEnabled = true;
+            ButtonState.IsEEnabled = true;
+            ButtonState.IsFEnabled = true;
+            ButtonState.Is0Enabled = true;
+            ButtonState.Is1Enabled = true;
+            ButtonState.Is2Enabled = true;
+            ButtonState.Is3Enabled = true;
+            ButtonState.Is4Enabled = true;
+            ButtonState.Is5Enabled = true;
+            ButtonState.Is6Enabled = true;
+            ButtonState.Is7Enabled = true;
+            ButtonState.Is8Enabled = true;
+            ButtonState.Is9Enabled = true;
+        }
+
+        private void EnableDecButtons()
+        {
+            ButtonState.IsAEnabled = false;
+            ButtonState.IsBEnabled = false;
+            ButtonState.IsCEnabled = false;
+            ButtonState.IsDEnabled = false;
+            ButtonState.IsEEnabled = false;
+            ButtonState.IsFEnabled = false;
+            ButtonState.Is0Enabled = true;
+            ButtonState.Is1Enabled = true;
+            ButtonState.Is2Enabled = true;
+            ButtonState.Is3Enabled = true;
+            ButtonState.Is4Enabled = true;
+            ButtonState.Is5Enabled = true;
+            ButtonState.Is6Enabled = true;
+            ButtonState.Is7Enabled = true;
+            ButtonState.Is8Enabled = true;
+            ButtonState.Is9Enabled = true;
+        }
+
+        private void EnableOctButtons()
+        {
+            ButtonState.IsAEnabled = false;
+            ButtonState.IsBEnabled = false;
+            ButtonState.IsCEnabled = false;
+            ButtonState.IsDEnabled = false;
+            ButtonState.IsEEnabled = false;
+            ButtonState.IsFEnabled = false;
+            ButtonState.Is0Enabled = true;
+            ButtonState.Is1Enabled = true;
+            ButtonState.Is2Enabled = true;
+            ButtonState.Is3Enabled = true;
+            ButtonState.Is4Enabled = true;
+            ButtonState.Is5Enabled = true;
+            ButtonState.Is6Enabled = true;
+            ButtonState.Is7Enabled = true;
+            ButtonState.Is8Enabled = true;
+            ButtonState.Is9Enabled = false;
+        }
+
+        private void EnableBinButtons()
+        {
+            ButtonState.Is0Enabled = true;
+            ButtonState.Is1Enabled = true;
+            ButtonState.Is2Enabled = false;
+            ButtonState.Is3Enabled = false;
+            ButtonState.Is4Enabled = false;
+            ButtonState.Is5Enabled = false;
+            ButtonState.Is6Enabled = false;
+            ButtonState.Is7Enabled = false;
+            ButtonState.Is8Enabled = false;
+            ButtonState.Is9Enabled = false;
+            ButtonState.IsAEnabled = false;
+            ButtonState.IsBEnabled = false;
+            ButtonState.IsCEnabled = false;
+            ButtonState.IsDEnabled = false;
+            ButtonState.IsEEnabled = false;
+            ButtonState.IsFEnabled = false;
+        }
     }
 }
