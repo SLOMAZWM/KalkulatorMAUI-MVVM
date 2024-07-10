0243ce79367b98d99130c97b49b82479a7ccc2cc
Marcin.Slomka2001 <marcin.slomka2001@gmail.com>
2024-07-10 12:28:08 +0200

    Basic Api Use without view Binding (CurrencyView)

---
+++ Models/CurrencyRatesResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalkulatorMAUI_MVVM.Models
+{
+    public class CurrencyRatesResponse
+    {
+        public bool Success { get; set; }
+        public Dictionary<string, decimal> Rates { get; set;}
+        public string Base { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
--- ViewModels/CurrencyViewModel.cs
+++ ViewModels/CurrencyViewModel.cs
@@ -1,13 +1,83 @@
-﻿using System;
+﻿using KalkulatorMAUI_MVVM.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace KalkulatorMAUI_MVVM.ViewModels
 {
-    public class CurrencyViewModel : ObservableObject
+    public partial class CurrencyViewModel : ObservableObject
     {
+        private readonly HttpClient _httpClient;
+        private const string ApiUrl = "https://api.apilayer.com/exchangerates_data/latest?base=USD";
 
+        [ObservableProperty]
+        private string _selectedCurrencyFrom;
+
+        [ObservableProperty]
+        private string _selectedCurrencyTo;
+
+        [ObservableProperty]
+        private string _displayCurrencyFrom = "";
+
+        [ObservableProperty]
+        private string _displayCurrencyTo = "";
+
+        [ObservableProperty]
+        private string _displayCurrentExchangeRate = "";
+
+        [ObservableProperty]
+        private string _displayLastUpdate = "";
+
+        [ObservableProperty]
+        private List<string> _availableCurrencies;
+
+        public CurrencyViewModel()
+        {
+            AvailableCurrencies = new List<string>();
+            _httpClient = new HttpClient();
+            _httpClient.DefaultRequestHeaders.Add("apikey", "846bdfa23944086ec19dc33dfde9287c");
+        }
+
+        [RelayCommand]
+        private async Task LoadCurrenciesAsync()
+        {
+            try
+            {
+                var response = await _httpClient.GetFromJsonAsync<CurrencyRatesResponse>(ApiUrl);
+                if(response != null && response.Success)
+                {
+                    AvailableCurrencies = response.Rates.Keys.ToList();
+                    DisplayLastUpdate = response.Date.ToString("yyyy-MM-dd HH:mm:ss");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Błąd: " + ex.Message);
+            }
+        }
+
+        private void UpdateExchangeRate()
+        {
+            if (string.IsNullOrEmpty(SelectedCurrencyFrom) || string.IsNullOrEmpty(SelectedCurrencyTo)) return;
+
+            var rate = GetExchangeRate(SelectedCurrencyFrom, SelectedCurrencyTo);
+            DisplayCurrencyFrom = SelectedCurrencyFrom;
+            DisplayCurrencyTo = SelectedCurrencyTo;
+            DisplayCurrentExchangeRate = $"{rate} {SelectedCurrencyTo}";
+        }
+
+        private decimal GetExchangeRate(string fromCurrency, string toCurrency)
+        {
+            var response = _httpClient.GetFromJsonAsync<CurrencyRatesResponse>(ApiUrl).Result;
+
+            if(response.Rates.TryGetValue(toCurrency, out var rate))
+            {
+                return rate;
+            }
+            return 1.0M;
+        }
     }
 }
