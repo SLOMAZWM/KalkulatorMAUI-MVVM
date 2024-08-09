0da3b4469f51773449a61e4d4d3ce8e56d98acf1
Marcin.Slomka2001 <marcin.slomka2001@gmail.com>
2024-08-09 22:32:15 +0200

    MemoryService prototype created

---
+++ Services/MemoryService.cs
@@ -0,0 +1,41 @@
+﻿namespace KalkulatorMAUI_MVVM.Services
+{
+    public static class MemoryService
+    {
+        private static Stack<long> _memoryStack = new Stack<long>();
+
+        public static void MemoryStore(long value)
+        {
+            _memoryStack.Push(value);
+        }
+
+        public static void MemoryAdd(long value)
+        {
+            long newValue = _memoryStack.Pop();
+            newValue = newValue + value;
+            _memoryStack.Push(newValue);
+        }
+
+        public static void MemorySubtract(long value)
+        {
+            long newValue = _memoryStack.Pop();
+            newValue = newValue - value;
+            _memoryStack.Push(newValue);
+        }
+
+        public static void MemoryClear()
+        {
+            _memoryStack.Clear();
+        }
+
+        public static long MemoryRecall()
+        {
+            return _memoryStack.Count > 0 ? _memoryStack.Peek() : 0;
+        }
+
+        public static List<long> GetMemoryStack()
+        {
+            return _memoryStack.ToList();
+        }
+    }
+}
